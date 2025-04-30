5eacaf10ad0d7569de54361ac258baa4cc235bac
Laastrog <laastrog@yandex.ru>
2025-04-30 18:29:55 +0300

    NEW ISSUE

---
+++ src/components/navigation.tsx
@@ -0,0 +1,10 @@
+import { ReactNode } from "react";
+
+export default function Navigation({children}:{children:ReactNode}){
+    return (
+        <>
+            {children}
+        </>
+    )
+
+}
